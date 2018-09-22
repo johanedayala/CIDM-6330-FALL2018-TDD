812f4eb80e7370e303fe6bc0d1717e2b7867f8b1
johanedayala <johanedayala@gmail.com>
2018-09-22 13:35:28 -0500

    basic view works in minimal HTML

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CIDM-6330-FALL2018-TDD
 
 ## Chapter 1 setting up first functional test 
+## Chapter 2 setting up first functional test
+## Chapter 3 setting up first functional test
--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
 from lists.views import home_page
+from django.http import HttpRequest
 
 class HomePageTest(TestCase):
 
@@ -8,3 +9,13 @@ class HomePageTest(TestCase):
         found = resolve('/')  
         self.assertEqual(found.func, home_page)
 
+    def test_home_page_returns_correct_html(self):
+        #arrange
+        request = HttpRequest()  
+        response = home_page(request) 
+        #act 
+        html = response.content.decode('utf8')  
+        #assert
+        self.assertTrue(html.startswith('<html>'))  
+        self.assertIn('<title>To-Do lists</title>', html)  
+        self.assertTrue(html.endswith('</html>'))  
