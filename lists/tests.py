ec66ffb17fbd9f36e32c804d0cd8d4fae1f64375
johanedayala <johanedayala@gmail.com>
2018-09-30 14:24:40 -0500

    updates for testing in chapter 4 using templates

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.urls import resolve
 from django.test import TestCase
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
 
@@ -10,12 +11,11 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        #arrange
-        request = HttpRequest()  
-        response = home_page(request) 
-        #act 
+        response = self.client.get('/')  
+
         html = response.content.decode('utf8')  
-        #assert
-        self.assertTrue(html.startswith('<html>'))  
-        self.assertIn('<title>To-Do lists</title>', html)  
-        self.assertTrue(html.endswith('</html>'))  
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.strip().endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'home.html')  
